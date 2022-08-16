0011231ccb68c1685e0c4603e008e43563a8426f
Giraldi Maggio <giraldi.maggio@yahoo.com>
2022-08-16 11:02:54 +0700

    prepare base files & folders

---
+++ src/navigation/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import * as React from 'react';
+
+const Navigation = () => {
+  return (
+    <View>
+      <Text>Navigation</Text>
+    </View>
+  )
+}
+
+export default Navigation
+
+const styles = StyleSheet.create({})
